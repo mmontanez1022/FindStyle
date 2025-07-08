fb67c60598c172690f083735dd4d0e31f1d2e278
mmontanez1022 <feliz.jueves22@gmail.com>
2025-07-08 00:59:38 -0500

    Cambios Recientes

--- servicios/forms.py
+++ servicios/forms.py
@@ -4,9 +4,9 @@ from .models import Servicios
 class ServiciosForm(forms.ModelForm):
     class Meta:
         model = Servicios
-        fields = ['title', 'description', 'important', 'imagen'] 
+        fields = ['title', 'description','imagen', 'precio'] 
         widgets ={
             'title' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Escribe un titulo'}),
             'description' : forms.Textarea(attrs={'class':'form-control', 'placeholder':'Escribe una descripción'}),
-            'important' : forms.CheckboxInput(attrs={'class':'form-check-input m-auto'}),
+            
         }
--- servicios/models.py
+++ servicios/models.py
@@ -6,10 +6,9 @@ class Servicios(models.Model): #1,05
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    datecompleted = models.DateTimeField(null=True, blank=True)
-    important = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     imagen = models.ImageField(upload_to='imagenes_productos/', null=True, blank=True)
+    precio = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
     
     def __str__(self):
         return self.title + '- by ' + self.user.username
